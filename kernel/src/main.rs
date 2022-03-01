ccfef2f3d05dbd7b93dce0f427f1d3278c756a0e
watasuke102 <watasuke102@gmail.com>
2022-03-01 15:33:15 +0900

    update: なにこれ

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -23,22 +23,6 @@ pub extern "sysv64" fn kernel_main(
   _memmap: u64,
   _acpi_table: u64,
 ) -> ! {
-  let frame_manager = FrameManager {
-    active_container: match Arc::new(Window {
-      color: PixelColor::from_hex(0x1f24b6),
-    }) {
-      Ok(a) => a,
-      Err(_) => {
-        config.write_rect(
-          Vec2::<u32> { x: 0, y: 0 },
-          config.size(),
-          &PixelColor::from_hex(0xff0000),
-          true,
-        );
-        panic!();
-      }
-    },
-  };
   // config.write_pixel(Vec2::<u32> { x: x, y: y }, &PixelColor::from_hex(0x32a852));
   config.write_rect(
     Vec2::<u32> { x: 0, y: 0 },
--- kernel/src/window.rs
+++ kernel/src/window.rs
@@ -6,31 +6,29 @@ use heapless::{
 };
 use kernel::{Direction, Vec2};
 
-trait Frame {
-  fn draw();
-}
-
 pub struct Window {
-  //parent_container: Arc<FrameContainer>,
+  valid:     bool,
   pub color: PixelColor,
 }
 
 pub struct FrameContainer {
-  //list:             Queue<Arc<dyn Frame>, 16>,
-  pub direction:        Direction,
-  pub parent_container: Arc<FrameContainer>,
-  pub size:             Vec2<u32>,
+  valid:         bool,
+  list:          Queue<Frame, 16>,
+  pub direction: Direction,
+  pub size:      Vec2<u32>,
 }
 
-impl Pool for FrameContainer {
-  type Data = Window;
-  fn ptr() -> &'static heapless::pool::Pool<ArcInner<<window::Window>::Data>> {
-    static POOL: heapless::pool::Pool<ArcInner<<window::Window>::Data>> =
-      heapless::pool::Pool::new();
-    &POOL
-  }
+enum FrameType {
+  FrameContainer,
+  Window,
+}
+
+struct Frame {
+  frame_type: FrameType,
+  container:  FrameContainer,
+  window:     Window,
 }
 
 pub struct FrameManager {
-  pub active_container: Arc<FrameContainer>,
+  pub active_container: Frame,
 }
