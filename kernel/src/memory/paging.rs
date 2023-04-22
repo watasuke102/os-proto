ff2e672803c3dc95ba30011c201697ffcabc64fc
watasuke102 <watasuke102@gmail.com>
2023-04-22 15:57:14 +0900

    refactor: use correct name about Paging

--- kernel/src/memory/paging.rs
+++ kernel/src/memory/paging.rs
@@ -1,3 +1,4 @@
+use common::serial_println;
 use x86_64::{
   registers::control::{Cr3, Cr3Flags},
   structures::paging::*,
@@ -7,9 +8,9 @@ use x86_64::{
 const EMPTY_PAGE_TABLE: PageTable = PageTable::new();
 const PAGE_DIR_COUNT: usize = 64;
 
-static mut L4_TABLE: PageTable = EMPTY_PAGE_TABLE;
-static mut DIR_POINTER_TABLE: PageTable = EMPTY_PAGE_TABLE;
-static mut PAGE_DIR: [PageTable; PAGE_DIR_COUNT] = [EMPTY_PAGE_TABLE; PAGE_DIR_COUNT];
+static mut PML4_TABLE: PageTable = PageTable::new();
+static mut PDP_TABLE: PageTable = PageTable::new();
+static mut PAGE_DIRECTORY: [PageTable; PAGE_DIR_COUNT] = [EMPTY_PAGE_TABLE; PAGE_DIR_COUNT];
 
 fn phys_frame_from_table(table: &PageTable) -> PhysFrame {
   PhysFrame::from_start_address(PhysAddr::new((table as *const PageTable) as u64)).unwrap()
@@ -18,17 +19,19 @@ pub fn init() {
   let page_table_flag = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
 
   unsafe {
-    L4_TABLE[0].set_frame(phys_frame_from_table(&DIR_POINTER_TABLE), page_table_flag);
-    for (i, page_dir) in PAGE_DIR.iter_mut().enumerate() {
-      DIR_POINTER_TABLE[i].set_frame(phys_frame_from_table(&page_dir), page_table_flag);
-      for (j, dir_entry) in PAGE_DIR[i].iter_mut().enumerate() {
+    PML4_TABLE[0].set_frame(phys_frame_from_table(&PDP_TABLE), page_table_flag);
+    // PageTable * 64
+    for (i, page_directory) in PAGE_DIRECTORY.iter_mut().enumerate() {
+      PDP_TABLE[i].set_frame(phys_frame_from_table(&page_directory), page_table_flag);
+      // Page Entry * 512 (2MiB * 512 == 1024MiB == 1GiB)
+      for (j, page_table) in PAGE_DIRECTORY[i].iter_mut().enumerate() {
         let addr = (i as u64 * Size1GiB::SIZE) + (j as u64 * Size2MiB::SIZE);
-        dir_entry.set_addr(
+        page_table.set_addr(
           PhysAddr::new(addr),
           page_table_flag | PageTableFlags::HUGE_PAGE,
         );
       }
     }
-    Cr3::write(phys_frame_from_table(&L4_TABLE), Cr3Flags::empty());
+    Cr3::write(phys_frame_from_table(&PML4_TABLE), Cr3Flags::empty());
   }
 }
